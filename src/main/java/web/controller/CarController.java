ecd2c2d777912784111314a5968b24b9b0132e8d
xidef-fedix <109800756+xidef-fedix@users.noreply.github.com>
2022-10-17 12:04:07 +0300

    Добавил интерфейс CarService

--- src/main/java/web/controller/CarController.java
+++ src/main/java/web/controller/CarController.java
@@ -1,16 +1,20 @@
 package web.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import web.service.CarService;
+import web.service.CarServiceImpl;
 
 @Controller
 public class CarController {
+    @Autowired
+    private CarService carService;
     @GetMapping("/cars")
     public String printCars(Model model, @RequestParam(value = "count", required = false) Integer count) {
-        CarService carService = new CarService();
+
         if (count != null) {
             model.addAttribute("carlist", carService.getCar(count));
         } else {
